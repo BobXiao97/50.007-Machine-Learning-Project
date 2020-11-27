4acab2fc626236f2976baab759c8900e0022e604
BobXiao97 <xtq1997@gmail.com>
2020-11-27 19:15:19 +0800

    grid for viterbi created

--- EN/part3.py
+++ EN/part3.py
@@ -21,4 +21,35 @@ def transition_parameter(data):
     transition['transition']=transition['Count_Yi_j']/transition['Count_Yi']
     return transition
 
+state=data['state']
+states=list(set(state))
+states.remove('Nil')
+UNK_list=[]
+for i in range(0,len(states)):
+    UNK_list.append('#UNK#')
+dic={'word':UNK_list,'state':states}
+data_UNK=pd.DataFrame(dic)
 
+def emission_parameter_UNK(data):
+    data=pd.concat([data,data_UNK])
+    data1=data['state'].value_counts().reset_index(name='CountY')
+    data1=data1.rename(columns={'index':'state'})
+    data2=data.groupby(['word','state']).size().reset_index(name='CountY_X')
+    data=pd.merge(data1,data2)
+    data=data[['word','state','CountY','CountY_X']]
+    data['CountY+k']=data['CountY']-0.5
+    data['emission']=data['CountY_X']/data['CountY+k']
+    for i in range(0,len(data['word'])):
+        if data['word'][i]=='#UNK#':
+            data['emission'][i]=data['emission'][i]/2
+    return data
+
+tran=transition_parameter(data)
+emi=emission_parameter_UNK(data).rename(columns={'state':"Yi"})
+parameter=pd.merge(tran,emi)
+parameter=parameter.drop(['Count_Yi','Count_Yi_j','CountY','CountY+k','CountY_X'],axis=1)
+parameter=parameter[['word','Yi','Yj','transition','emission']]
+print(parameter)
+def viterbi(parameters,input_data):
+    pass
+    
