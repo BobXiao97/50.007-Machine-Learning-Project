732653591934b92dba473b4b88dc313400cc036d
BobXiao97 <xtq1997@gmail.com>
2020-12-11 13:05:31 +0800

    part 5 done

---
+++ Test/readme.txt
@@ -0,0 +1,19 @@
+Part 2:
+Run 'part2.py' which generates 'dev.p2.out', the output is the result. It takes about 7 min.
+
+Part 3:
+1. Run 'part3 parameters.py' which generates 'emission parameter.csv' and 'transition parameter.csv'. It takes about 5 min.
+2. Run 'part3 viterbi.py' which generates 'dev.p3.out'. It takes about 10 min.
+3. Run 'part3 evaluation.py', the output is the result.
+You can also run 'part3 evaluation' directly as all the files needed for it is already in the folder.
+
+Part1 of Part 5:
+1. Run 'parameters.py' which generates 'emission parameter.csv' and 'transition parameter.csv'. It takes about 5 min.
+2. Run 'viterbi1.py' which generates 'dev.p5.out'. It takes about 12 min. 
+3. Run 'evaluation.py', the output is the result.
+You can also run 'evaluation' directly as all the files needed for it is already in the folder.
+
+Part 2 of Part 5:
+1. Run 'parameters.py' which generates 'emission parameter.csv' and 'transition parameter.csv'. It takes about 5 min.
+2. Run 'viterbi2.py' which generates 'test.p5.out'. It takes about 10 min. 
+You can also run 'viterbi2' directly as all the files needed for it is already in the folder.
---
+++ Test/viterbi2.py
@@ -0,0 +1,70 @@
+import pandas as pd
+import numpy as np
+
+def viterbi(obs,states,trans_p,emit_p):
+    v=[]
+    fst=obs[0]
+    vn=emit_p[fst]
+    v.append(vn)
+ 
+    for t in range(1,len(obs)):
+        tp=emit_p[obs[t]]
+        cc=[]
+        for y in states:
+            gv=v[t-1]*trans_p[y]*tp.loc[y]
+            cc.append(gv.max())
+        cc1=pd.Series(cc,index=states)
+        v.append(cc1)
+ 
+    result=[{'Nil':'Nil'}]
+    for i in range(len(v)-2,-1,-1):
+        b=list(result[-1])
+        a=v[i]*trans_p[b[0]]
+        p1=a.sort_values(ascending=False)
+        p2=p1[:1]
+        result.append(dict(p2))
+    return result
+
+data=pd.read_csv('train_EN',sep=' ',names=['word','state'],skip_blank_lines=False)
+data=data.fillna('Nil')
+observation=data['word']
+state=data['state']
+states=list(set(state))
+
+dev_in=pd.read_csv('test.in',sep=' ',names=['word'],skip_blank_lines=False)
+dic={'word':['Nil']}
+first_line=pd.DataFrame(dic)
+dev_in=pd.concat([first_line,dev_in],ignore_index=True)
+dev_in_list=np.split(dev_in,dev_in[dev_in.isnull().all(1)].index)
+for i in range(1,len(dev_in_list)):
+    dev_in_list[i]=dev_in_list[i].fillna('Nil')
+for j in range(0,len(dev_in_list)-1):
+    dev_in_list[j]=pd.concat([dev_in_list[j],first_line],ignore_index=True)
+
+for i in range(0,len(dev_in_list)):
+    dev_in_list[i]=dev_in_list[i]['word'].to_list()
+    for j in range(0,len(dev_in_list[i])):
+        if dev_in_list[i][j] not in observation.to_list():
+            dev_in_list[i][j]='#UNK#'
+trans_p=pd.read_csv('transition parameter.csv',index_col=0)
+emi_p=pd.read_csv('emission parameter.csv',index_col=0)
+
+key=[]
+for i in range(0,len(dev_in_list)):
+    result=viterbi(dev_in_list[i],states,trans_p,emi_p)
+    temp=[]
+    for j in range(0,len(result)):
+        k=list(result[j].keys())
+        temp.append(k[0])
+    temp=list(reversed(temp))
+    key.append(temp)
+
+with open('test.p5.out','w') as f:
+    for i in range(0,len(key)):
+        if len(key[i])>1:
+            for j in range(1,len(key[i])):
+                if key[i][j]!='Nil':
+                    f.write(key[i][j]+'\n')
+                else:
+                    f.write('\n')
+f.close 
